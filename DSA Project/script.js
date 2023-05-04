42644f4856a26a070520767dee4007b9eb36eb63
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-05-05 01:42:30 +0600

    add

--- DSA Project/script.js
+++ DSA Project/script.js
@@ -1,8 +1,51 @@
 const n =10;
 const arr = [];
-for(let i = 0; i < n; i++){
-    arr[i] =Math.random();
+
+
+function init(){
+    for(let i = 0; i < n; i++){
+        arr[i] =Math.random();
+    } 
 }
+
+
+
+        /*
+            for (var i = 0; i < arr.length; i++) {
+  
+            // Last i elements are already in place  
+            for (var j = 0; j < (arr.length - i - 1); j++) {
+      
+                // Checking if the item at present iteration 
+                // is greater than the next iteration
+                if (arr[j] > arr[j + 1]) {
+      
+                    // If the condition is true
+                    // then swap them
+                    var temp = arr[j]
+                    arr[j] = arr[j + 1]
+                    arr[j + 1] = temp
+                }
+            }
+        }
+        
+        */
+
+        let k = arr.length;  
+        let temp = 0;  
+         for(let i=0; i < k; i++){  
+                 for(let j=1; j < (k-i); j++){  
+                          if(arr[j-1] > arr[j]){  
+                                 //swap elements  
+                                 temp = arr[j-1];  
+                                 arr[j-1] = arr[j];  
+                                 arr[j] = temp;  
+                         }  
+                          
+                 }  
+         }  
+
+
 
 for(let i = 0; i < arr.length; i++){
     const bar = document.createElement('div');
