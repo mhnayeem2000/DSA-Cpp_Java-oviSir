1b005d32b39b3fe1daeb29d6aebfe6973e61f27e
mhnayeem2000 <mhnayeem018460@gmail.com>
2023-05-05 02:18:57 +0600

    Everything is fine

--- DSA Project/script.js
+++ DSA Project/script.js
@@ -10,23 +10,44 @@ function init(){
 }
 
 function play(){
-    bubbleSort(arr);
-    showBars();
+   const swaps= bubbleSort(arr);
+   animate(swaps);
 }
 
 
-function bubbleSort(arr){   
+function animate(swaps){
+    if(swaps.length == 0){
+        return;
+    }
+    const [i,j] = swaps.shift();
+    const bar1 = document.querySelectorAll('.bar')[i];
+    const bar2 = document.querySelectorAll('.bar')[j];
+    const height1 = bar1.style.height;
+    const height2 = bar2.style.height;
+    bar1.style.height = height2;
+    bar2.style.height = height1;
+    setTimeout(function() {
+        animate(swaps);
+    }, 100);
+}
+
+
+
+function bubbleSort(arr){  
+    const swaps = []; 
     let k = arr.length;  
     let temp = 0;  
     for(let i=0; i < k; i++){  
         for(let j=1; j < (k-i); j++){  
-            if(arr[j-1] > arr[j]){  
+            if(arr[j-1] > arr[j]){ 
+                swaps.push([j-1,j]); 
                 temp = arr[j-1];  
                 arr[j-1] = arr[j];  
                 arr[j] = temp;  
-            }              
+            }
         }  
     }
+    return swaps;
 }       
 
 
